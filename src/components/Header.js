40851b38920ccc6222c4ac40acb0764299ff40cc
adisunjana <adisunjana443@gmail.com>
2025-01-14 18:30:58 +0700

    add logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 const Header = ({ balance }) => {
+
+    const handleLogout = () => {
+        localStorage.setItem('isLoggedIn', false);
+        window.location.reload();
+    };
+
     return (
         <div className="text-center mt-3">
             <h2>Aplikasi Keuangan</h2>
             <h4>Total Saldo: Rp {balance.toLocaleString()}</h4>
+
+            <button className="btn btn-danger" onClick={handleLogout}>
+                Logout
+            </button>
         </div>
     );
 };
