6321b3fd5b6fdcea43bfa45feac3e0c62df958f8
adisunjana <adisunjana443@gmail.com>
2025-01-14 17:26:47 +0700

    update app js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+import React, { useState } from 'react';
+import Header from './components/Header';
+import AddTransaction from './components/AddTransaction';
+import TransactionList from './components/TransactionList';
+
+const App = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  const calculateBalance = () => {
+    return transactions.reduce((total, transaction) => {
+      // console.log('Transaction ==> ', transaction)
+      // console.log('total ==> ', total)
+      // console.log('amount ==> ', transaction.amount)
+      return total + transaction.amount
+    }, 0);
+  };
+
+  const addTransaction = (transaction) => {
+    console.log(transaction)
+    setTransactions([transaction, ...transactions]);
+  };
+
+  const removeTransaction = (index) => {
+    const updatedTransactions = transactions.filter((_, i) => i !== index);
+    setTransactions(updatedTransactions);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <Header balance={calculateBalance()} />
+      <AddTransaction onAddTransaction={addTransaction} />
+      <TransactionList
+        transactions={transactions}
+        onRemoveTransaction={removeTransaction}
+      />
     </div>
   );
-}
+};
 
 export default App;
